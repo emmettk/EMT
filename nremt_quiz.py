8b31bd490b9977bfa7b0ac60626229a9bd61db55
emmett-pvpiv <emmett@whoi.edu>
2018-03-19 00:07:10 -0400

    Quiz mechanism update
    No longer requires pandas. Cleaned up and consolidated quiz mechanisms.

--- nremt_quiz.py
+++ nremt_quiz.py
@@ -4,111 +4,112 @@ Created on Thu Mar 15 10:48:16 2018
 
 @author: E. Krupczak
 
-A little quiz program for the NREMT
+A little quiz program for the NREMT exam
 Covers GCS, APGAR, and rule of nines
 
-Requires pandas (quiz info stored as pandas dataframe)
+No effort has been made to have the scores in each category correlate,
+i.e. don't ask me to explain the infant with no pulse but a strong cry! :) 
+
 """
 
-import pandas as pd
 import random
 
-#Make compatible with python2
+#Make input compatible with python2
 try: 
     get_input = raw_input
 except NameError: 
     get_input = input
 
 #Quiz data
-GCS = pd.DataFrame( {1: ["Does not open eyes", "Makes no sounds", "Makes no movements"], 
-                     2: ["Opens eyes in response to pain", "Incomprehensible sounds", "Extension to pain (decerebrate)"],
-                     3: ["Opens eyes in response to voice", "Incoherent words", "Abnormal flexion to pain (decorticate)"],
-                     4: ["Opens eyes spontaneously", "Confused/disoriented", "Flexion/withdrawal to pain"],
-                     5: [None, "Oriented", "Localizes to painful stimuli"],
-                     6: [None, None, "Obeys commands"]}, 
-                        index = ["Eyes", "Verbal", "Motor"])
+GCS = {"Eyes"  : [("Does not open eyes", 1), ("Opens eyes in response to pain",2), 
+                  ("Opens eyes in response to voice",3), ("Opens eyes spontaneously",4)],
+       "Verbal": [("Makes no sounds",1), ("Incomprehensible sounds",2), 
+                  ("Incoherent words",3), ("Confused/disoriented",4), ("Oriented",5)],
+       "Motor" : [("Makes no movements",1), ("Extension to pain (decerebrate)",2),
+                  ("Abnormal flexion to pain (decorticate)",3), ("Flexion/withdrawal to pain",4),
+                  ("Localizes to painful stimuli",5), ("Obeys commands",6)]}
 
-
-APGAR = pd.DataFrame({0: ["Blue or pale all over", "Absent pulse", "No response to stimulation", "No activity", "No respirations"],
-                      1: ["Pink body; blue at extremities (acrocyanosis)", "Pulse <100 bpm", "Grimace on stimulation", "Some flexion", "Weak/irregular/gasping respirations"],
-                      2: ["No cyanosis", "Pulse >100 bpm", "Cry on stimulation", "Flexed arms and legs that resist extension", "Strong cry"]}, 
-                         index = ["Appearance", "Pulse", "Grimace", "Activity", "Respiration"])
+APGAR = {"Appearance" : [("Blue or pale all over",0), ("Pink body; blue at extremities (acrocyanosis)",1), ("No cyanosis",2)],
+         "Pulse"      : [("Absent pulse",0), ("Pulse <100 bpm",1), ("Pulse >100 bpm",2)],
+         "Grimace"    : [("No response to stimulation", 0), ("Grimace on stimulation", 1), ("Cry on stimulation", 2)],
+         "Activity"   : [("No activity", 0), ("Some flexion", 1), ("Flexed arms and legs that resist extension", 2)],
+         "Respiration": [("No respirations",0), ("Weak/irregular/gasping respirations",1), ("Strong cry", 2)]}
 
 nines_adult = {"Head": 9, "Arm": 9, "Torso": 36, "Leg": 18, "Genitals": 1}
 nines_peds = {"Head": 18, "Arm": 9, "Torso": 36, "Leg": 14, "Genitals": 1}
 
 
-#Quiz mechanism: Provide one element from each row, calculate score from column name
-#GCS and APGAR mechanisms are basically the same
-def GCS_quiz():
+def quiz_mechanism(quiz_dict):
+    """
+    Quiz mechanism for GCS and APGAR 
+    Randomly select an element (tuple) from each key in quiz dict
+    Return question string, answer string, and answer score
+    """   
+    question = []
+    answer = []
+    for k,v in quiz_dict.items():
+        element = random.choice(v)
+        question.append(element[0])
+        answer.append(element[1])
+    return ", ".join(question), "+".join([str(i) for i in answer])+"="+str(sum(answer)), sum(answer)
+
+def quiz_display(quiz_dict, name):
+    """
+    Input/output for a quiz using the given quiz dictionary. 
+    Works for GCS and APGAR
+    """
     quizmode = "y"
     while quizmode == "y":
-        Eyes = random.randrange(1,5)
-        Verbal = random.randrange(1,6)
-        Motor = random.randrange(1,7)
-    
-        print(GCS.loc["Eyes", Eyes]+", "+ GCS.loc["Verbal", Verbal]+", "+ GCS.loc["Motor", Motor])
-        score = get_input("Please enter GCS score: ")
+        question, answer, answer_score = quiz_mechanism(quiz_dict)
+        print(question)
+        score = get_input("Please enter "+name+" score for the patient described above: ")
         try:
-            if int(score) == Eyes+Verbal+Motor: print("Correct!")
+            if int(score) == answer_score: print("Correct!")
             else: print("Incorrect.")
         except ValueError: 
             print("Score must be an integer")
-        print("GCS score: "+ str(Eyes)+ "+"+ str(Verbal)+"+"+str(Motor)+ "="+ str(Eyes+Verbal+Motor))
+        print(name+" score: "+answer)
         quizmode = get_input("Enter 'y' for another question, or anything else to exit. ")
         
-def APGAR_quiz():
-    quizmode = "y"
-    while quizmode == "y":
-        Ap = random.randrange(0,3)
-        P = random.randrange(0,3)
-        G = random.randrange(0,3)
-        Ac = random.randrange(0,3)
-        R = random.randrange(0,3)
         
-        print(APGAR.loc["Appearance", Ap]+", "+ APGAR.loc["Pulse", P]+", "+ APGAR.loc["Grimace",G]+ ", "+ APGAR.loc["Activity", Ac]+ ", "+ APGAR.loc["Respiration", R])
-        score = get_input("Please enter APGAR score: ")
-        try:
-            if int(score) == Ap+P+G+Ac+R: print("Correct!")
-            else: print("Incorrect.")
-        except ValueError: 
-            print("Score must be an integer")
-        print("APGAR score: "+ str(Ap)+ "+"+str(P)+"+"+str(G)+"+"+str(Ac)+"+"+str(R)+ "=" + str(Ap+P+G+Ac+R))
-        quizmode = get_input("Enter 'y' for another question, or anything else to exit. ")
-        
-#The Rule of Nines quiz mechanism is a little messy. Allows for anterior, posterior or entire surface to be burned.
+#The Rule of Nines quiz mechanism is a little messy. 
 def nines_quiz():
+    """
+    Rule of Nines quiz.
+    Allows for anterior, posterior or entire surface to be burned.
+    Randomly selects pediatric or adult.
+    """
     quizmode = "y"
     while quizmode == "y":
         planes = ["Anterior", "Posterior", "Entire"]
         sides = ["Left", "Right"]
+        #List all possible body parts that can be burned
         parts_list = [" ".join((k,l)) for k in sides for l in ["Arm", "Leg"]]+["Head", "Torso", "Genitals"]
-        #parts_list = [" ".join((i,j)) for i in planes for j in ["Head", "Torso"]+[" ".join((k,l)) for k in sides for l in ["Arm", "Leg"]]]
+        #Choose which parts are burned
         burned_parts = random.sample(parts_list, random.randrange(1, len(parts_list)))
-        #burned_surface = [" ".join((i,j)) for i in random.sample(planes,1) for j in burned_parts]
+        #Choose if each part is burned on anterior, posterior or both
         burned_surface =[]
         for j in burned_parts:
             burned_surface +=[" ".join((i,j)) for i in random.sample(planes,1)]
-        
-        #Select peds or adult
-        pedsvsadult = random.randrange(0,2) #0=peds, 1 = adult
-        burn_score = {}
+        #Calculate the percentage burned from the list of burned parts
+        def get_burn_score(burned_surface, nines_dict):
+            burn_score = {}
+            for part in burned_surface:
+                for key in nines_dict.keys():
+                    if key in part:
+                        if "Entire" in part: #The whole area is burned
+                            burn_score[part] = float(nines_dict[key])
+                        else: #Anterior or posterior: half the area is burned
+                            burn_score[part] = nines_dict[key]/2.0
+            return burn_score
+        #Select pediatric or adult
+        pedsvsadult = random.randint(0,1) #0=peds, 1 = adult        
         if pedsvsadult: #adult
-            for part in burned_surface:
-                for key in nines_adult.keys():
-                    if key in part:
-                        if "Entire" in part:
-                            burn_score[part] = nines_adult[key]
-                        else:
-                            burn_score[part] = nines_adult[key]/2.0
-        else: #peds
-            for part in burned_surface:
-                for key in nines_peds.keys():
-                    if key in part:
-                        if "Entire" in part:
-                            burn_score[part] = nines_peds[key]
-                        else:
-                            burn_score[part] = nines_peds[key]/2.0  
+            burn_score = get_burn_score(burned_surface, nines_adult)
+        else: #pediatric
+            burn_score = get_burn_score(burned_surface, nines_peds)
+        
+        #Print the quiz
         if pedsvsadult: 
             print("An adult has burned: "+ ", ".join(burned_surface))
         else:
@@ -118,7 +119,7 @@ def nines_quiz():
             if float(score) == sum(burn_score.values()): print("Correct!")
             else: print("Incorrect")
         except ValueError:
-            print("Burn percentage must be a number")
+            print("Burn percentage must be a number.")
         print("Percentage burned: "+ str(sum(burn_score.values())))
         for k, v in burn_score.items():
             print(k+": "+ str(v))
@@ -127,9 +128,9 @@ def nines_quiz():
 if __name__== "__main__":
     quiz = get_input("Pick a quiz (GCS, APGAR, nines): ")
     if quiz == "GCS":
-        GCS_quiz()
+        quiz_display(GCS, quiz)
     elif quiz == "APGAR":
-        APGAR_quiz()
+        quiz_display(APGAR, quiz)
     elif quiz == "nines":
         nines_quiz()
     else: print("Invalid entry. Please try again and enter 'GCS', 'APGAR' or 'nines'.")
